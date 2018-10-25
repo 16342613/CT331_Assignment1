96e61932cbc6250e5eb9a74569b53fe3d069e470
Mandhu Laptop <36735922+IGDB36@users.noreply.github.com>
2018-10-25 22:38:10 +0100

    update

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,22 @@
+// Name: Irish Senthilkumar
+// ID: 16342613
+// Details: CT331 Programming Paradigms Assignment 1 Q1
+
 #include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+int main(int arg, char* argc[]) {
+	// instantiate our instance variables
+	int intType;
+	int* intPointerType;
+	long longType;
+	double* doublePointerType;
+	char** characterPointerPointerType;
+
+	printf("\n"); // Line break
+	printf("The size of type int is %d bytes\n", sizeof(intType));  // Get the size of an int
+	printf("The size of type int pointer is %d bytes\n", sizeof(intPointerType));  // Get the size of an int pointer
+	printf("The size of type long is %d bytes\n", sizeof(longType));  // Get the size of a long
+	printf("The size of type double pointer is %d bytes\n", sizeof(doublePointerType));  // Get the size of a double pointer
+	printf("The size of type character pointer pointer is %d bytes\n", sizeof(characterPointerPointerType));  // Get the size of a character pointer pointer
+	printf("\n"); // Line break
 }
