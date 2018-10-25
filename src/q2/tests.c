fce4bdeb5c6777bed51030e76c94ddc72ebfb08f
Mandhu Laptop <36735922+IGDB36@users.noreply.github.com>
2018-10-25 21:31:43 +0100

    update

--- src/q2/tests.c
+++ src/q2/tests.c
@@ -53,31 +53,21 @@ void testThree() {
 	enqueue(&queueHead, "2nd queued", 30);
 	enqueue(&queueHead, "3rd queued", 30);
 	printf("Here are the queued elements:\n");
+	traverse(queueHead);  // Traverse from the head node
+	int lengthOfList = length(queueHead);
+	printf("\nWe have %d elements in the queue\n", lengthOfList);
+	printf("\nNow I'm going to dequeue 2 elements:\n");
+	listElement* dequeuedElement = dequeue(queueHead);
+	listElement* dequeuedElement2 = dequeue(queueHead);
+	printf("\nHere is whats left in the queue:\n");
+	traverse(queueHead);
+	lengthOfList = length(queueHead);
+	printf("\nWe have %d elements in the queue\n", lengthOfList);
 }
 
 void runTests(){
-	
+	// Run the unit tests
 	testOne();
 	testTwo();
 	testThree();
-	listElement* stackHead = createEl("Initial stack node", 30);
-	listElement* queueHead = createEl("Initial queue node", 30);
-
-	push(&stackHead, "1st push", 30);
-	push(&stackHead, "2nd push", 30);
-	push(&stackHead, "3rd push", 30);
-	int lengthOfList = length(stackHead);
-	printf("\nThe length of the list is %d\n", lengthOfList);
-	listElement* popped = pop(&stackHead);
-	traverse(stackHead);
-
-	printf("\nQueue tests:\n");
-
-	enqueue(&queueHead, "1st queued", 30);
-	enqueue(&queueHead, "2nd queued", 30);
-	enqueue(&queueHead, "3rd queued", 30);
-	listElement* dequeuedElement = dequeue(queueHead);
-	listElement* dequeuedElement2 = dequeue(queueHead);
-
-	traverse(queueHead);
 }
